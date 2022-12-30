84852ee446b4abdc0d83bbea34401723880676b0
marcelo-m-filho <75740049+marcelo-m-filho@users.noreply.github.com>
2022-12-30 11:54:06 -0300

    Adição de funcionalidade UART (#1)
    * 🔥 removed autogenerated crc init files
    
    * 🧑‍💻 added project color
    
    * 🚧 added initial uart communication
    
    * 🚧 added initial variable counting in uart
    
    * 🧑‍💻 added vscode file config

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -40,8 +40,6 @@
 
 /* Private variables ---------------------------------------------------------*/
 
-CRC_HandleTypeDef hcrc;
-
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
@@ -51,7 +49,6 @@ UART_HandleTypeDef huart1;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_CRC_Init(void);
 static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
@@ -90,9 +87,12 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_CRC_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
+  uint8_t initString[] = "\r\n--- Apple Pie Initialization Complete! ---\r\n";
+  HAL_UART_Transmit(&huart1, initString, sizeof(initString), 10);
+
+  int32_t messageCounter = 0;
 
   /* USER CODE END 2 */
 
@@ -100,9 +100,12 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  uint8_t Test[] = "Hello World !!!\r\n"; //Data to send
-	  HAL_UART_Transmit(&huart1,Test,sizeof(Test),10);// Sending in normal mode
+	  uint8_t messageCounterString[20];
+    uint8_t stringSize = sprintf(messageCounterString, "Apple Pie %i!\r\n", messageCounter);
+	  HAL_UART_Transmit(&huart1, messageCounterString, stringSize, 10);
 	  HAL_Delay(1000);
+    messageCounter++;
+    
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -168,37 +171,6 @@ void SystemClock_Config(void)
     Error_Handler();
   }
   HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_HSI, RCC_MCODIV_1);
-}
-
-/**
-  * @brief CRC Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_CRC_Init(void)
-{
-
-  /* USER CODE BEGIN CRC_Init 0 */
-
-  /* USER CODE END CRC_Init 0 */
-
-  /* USER CODE BEGIN CRC_Init 1 */
-
-  /* USER CODE END CRC_Init 1 */
-  hcrc.Instance = CRC;
-  hcrc.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_ENABLE;
-  hcrc.Init.DefaultInitValueUse = DEFAULT_INIT_VALUE_ENABLE;
-  hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_NONE;
-  hcrc.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_DISABLE;
-  hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_BYTES;
-  if (HAL_CRC_Init(&hcrc) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN CRC_Init 2 */
-
-  /* USER CODE END CRC_Init 2 */
-
 }
 
 /**
